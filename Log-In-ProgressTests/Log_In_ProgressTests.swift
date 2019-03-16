b63df01e080b182a5c33e170f8b4ae4823f65ffc
Steve Lee <steve.lee2@mattel.com>
2019-03-16 00:51:54 -0700

    Initial Commit

---
+++ Log-In-ProgressTests/Log_In_ProgressTests.swift
@@ -0,0 +1,34 @@
+//
+//  Log_In_ProgressTests.swift
+//  Log-In-ProgressTests
+//
+//  Created by Lee, Steve on 3/16/19.
+//  Copyright © 2019 Lee, Steve. All rights reserved.
+//
+
+import XCTest
+@testable import Log_In_Progress
+
+class Log_In_ProgressTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
